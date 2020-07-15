ff0fe9ce22771eb8b33ae65a76049917bab550e3
Ynda Jas <yndajas@gmail.com>
2020-07-15 01:35:53 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.none? {|con_brand| con_brand == brand}
+      BRANDS << brand
+    end
   end
 
   def cobble
